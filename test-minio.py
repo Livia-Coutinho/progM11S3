fadc6cd067d23aa4e4e9aa3cfd1c6720d15c68c1
Livia Coutinho <99189965+Livia-Coutinho@users.noreply.github.com>
2024-08-25 20:47:25 -0300

    Update test-minio.py

--- test-minio.py
+++ test-minio.py
@@ -8,3 +8,31 @@ def test_minio():
         secret_key="minioadmin",
         secure=False
     )
+
+    bucket_name = "test_bucket"
+    object_name = "test.txt"
+    file_content = "Este é um arquivo teste"
+
+    try:
+        if not client.bucket_exists(bucket_name):
+            client.make_bucket(bucket_name)
+            print(f"Bucket '{bucket_name}' criado com sucesso")
+        else:
+            print(f"Bucket '{bucket_name}' já existe")
+
+        with open(object_name, "w") as file:
+            file.write(file_content)
+
+        #Upload
+        client.fput_object(bucket_name, object_name, object_name)
+        print(f"Arquivo '{object_name}' enviado com sucesso")
+
+        #Download
+        client.fget_object(bucket_name, object_name, f"downloaded_{object_name}")
+        print(f"Arquivo '{object_name}' download finalizado com sucesso")
+
+    except S3Error as e:
+        print(f"Arquivo '{e}' ")
+
+if __name__ == "__main__":
+    test_minio()
